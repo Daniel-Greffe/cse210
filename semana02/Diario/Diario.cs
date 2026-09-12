61393be92626512c81cfa1195673b926c48928cb
daniel-greffe <dcardoso2@byupathway.edu>
2026-09-11 23:22:06 -0400

    semana 2 - criação das classes

---
+++ semana02/Diario/Diario.cs
@@ -0,0 +1,24 @@
+public class Diario
+{
+    public List<Registro> _registros;
+
+    public void AdicionarRegistro(Registro novoRegistro)
+    {
+        _registros.Add(novoRegistro);
+    }
+
+    public void ExibirTodos()
+    {
+        
+    }
+
+    public void SalvarNoArquivo(string arquivo)
+    {
+        
+    }
+
+    public void CarregarDoArquivo(string arquivo)
+    {
+        
+    }
+}
